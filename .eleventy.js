26916510d7ee576da331b2860a7c5e144de22ae9
Hugh Collier <collier.289@gmail.com>
2024-02-13 17:07:34 +0000

    Dev server now shows local network IP addresses for device testing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/js');
+  
+  eleventyConfig.setServerOptions({
+    showAllHosts: true,
+  });
 
   return {
     dir: {
