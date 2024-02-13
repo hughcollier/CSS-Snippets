8713622612d1c5695b13b01e90454b65ac574ac1
Hugh Collier <collier.289@gmail.com>
2024-02-13 16:06:28 +0000

    Removed dist dir and added rule to ignore this dir

--- dist/js/script.js
+++
@@ -1,8 +0,0 @@
-const accordions = document.querySelectorAll('.accordion-title')
-
-accordions.forEach(accordion => {
-  accordion.addEventListener('click', function () {
-    accordion.classList.toggle('open')
-    accordion.nextElementSibling.classList.toggle('open')
-  })
-})
