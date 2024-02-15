b755b194c5ec6cda5189010c14558c1f5984a03b
Hugh Collier <collier.289@gmail.com>
2024-02-15 13:11:35 +0000

    Small changes to CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,26 @@
 module.exports = function (eleventyConfig) {
+  
+  // Make sure Eleventy is aware of our CSS and JS 
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/js');
   
+  eleventyConfig.addFilter("prependTitle", function(string, text) {
+    return `${text} ${string}`
+  });
+  
+  // Display links for local network device testing when dev server starts
   eleventyConfig.setServerOptions({
     showAllHosts: true,
   });
 
   return {
+    // Override default input / output directory names
     dir: {
       input: 'src',
       output: 'dist',
     },
+    
+    // Set templating language for .md files to nunjucks (default is liquid)
+    markdownTemplateEngine: "njk",
   };
 };
